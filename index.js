e7dc8217d2e0059722e2f6b608c3884f95bf6705
Alex Petrov <i@petrov.com.ru>
2019-01-11 16:12:59 +0300

    Функции, модули и директива require(), Множественный вывод из модуля

---
+++ index.js
@@ -0,0 +1,9 @@
+const things = require('./things');
+const {counter} = require('./things');
+
+console.log(things.some_value);
+console.log(counter([2, 5, 12, 2]));
+console.log(things.multiply(2, 3));
+
+// console.log('File name:', __filename);
+// console.log('Dir name:', __dirname);
---
+++ things.js
@@ -0,0 +1,15 @@
+const counter = (array) => {
+    return `There are ${array.length} elements in the array`;
+};
+
+const multiply = (x, y) => {
+    return `${x} times ${y} equle to ${x * y}`;
+}
+
+const some_value = 451
+
+module.exports = {
+    counter,
+    multiply,
+    some_value
+};
